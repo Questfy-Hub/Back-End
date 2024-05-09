61e6f998df843e1f8e444706019b5cb607f793ff
Gabriel Evaristo <gabrielousadia12@gmail.com>
2024-05-09 16:56:39 -0300

    Cross Origin adicionado
    Adicionado a Anotação CrossOrigin para consumo da API dentro do Front-End

--- app/src/main/java/com/questifyHub/app/Controllers/UserController.java
+++ app/src/main/java/com/questifyHub/app/Controllers/UserController.java
@@ -2,6 +2,7 @@ package com.questifyHub.app.Controllers;
 
 import com.questifyHub.app.Entities.User;
 import com.questifyHub.app.Repositories.UserRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class UserController {
     private final UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {this.userRepository = userRepository;}
-
+    @CrossOrigin("http://localhost:4200")
     @GetMapping("/users")
     public List<User> getUsers(){
         return this.userRepository.findAll();
