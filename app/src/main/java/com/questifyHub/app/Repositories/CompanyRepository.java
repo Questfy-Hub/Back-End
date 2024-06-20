7f303e0f645323b57f49eae99b16e46e43df6409
João Paulo <joao.paulo.pk66@gmail.com>
2024-06-20 14:22:50 -0300

    Mudança company repository

--- app/src/main/java/com/questifyHub/app/Repositories/CompanyRepository.java
+++ app/src/main/java/com/questifyHub/app/Repositories/CompanyRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.questifyHub.app.Entities.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-
+    Company getCompanyByCompanyCode(long companyCode);
 }
